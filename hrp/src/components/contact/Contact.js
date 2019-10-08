49e58bbbe4521e7c1ebcde878015b391ab4a19f3
Joni <joni.ylioja@gmail.com>
2019-10-08 14:34:59 +0300

    added home page text and edited footer and started contacts page

--- hrp/src/components/contact/Contact.js
+++ hrp/src/components/contact/Contact.js
@@ -1,14 +1,28 @@
-import React from 'react'
+import React from 'react';
 import './Contact.css'
 import NavBar from '../nav/NavBar'
+import { SocialIcon } from 'react-social-icons';
 
+     
 export default class Contact extends React.Component {
         render() {
             const title= "Yhteystiedot";
         return (
-            <div>
-                <NavBar title={title}/>    
-            </div>
+            <div className="contact">
+                <NavBar title={title}/>
+                <div className="vittumitapaskaa"> 
+                    <div class="p-2 flex-fill bd-highlight">
+                        <h1 className="text-left">Näin saavut Hiedanrantaan</h1>
+                        <h2 className="text-left">Kävellen ja pyörällä</h2>
+                        <h2 className="text-left">Bussilla</h2>
+                        <h2 className="text-left">Autolla</h2>
+                    </div>
+                    <div class="p-2 flex-fill bd-highlight">
+                        <h1 className="text-right">Yhteystiedot</h1>
+                    </div>
+                </div>
+                </div>
+                
         )
     }
 }
--- hrp/src/components/footer/Footer.js
+++ hrp/src/components/footer/Footer.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import './Footer.css';
-
+import { SocialIcon } from 'react-social-icons';
 const Footer  = () => {
     return (    
         <div className="footer">
             <div className="footerContent">
-                <p className="text-center">Hiedanrannan pajat on ihan best :)</p>
+                <p className="text-center"><span className="font-weight-bold">Hiedanrannan pajat</span> Tehdaskartanonkatu 28 33400 Tampere</p>
+                <p className="text-center">hiedanrannanpaja@gmail.com</p>
+                <div className="socials">
+                <SocialIcon className="facebook" url="http://facebook.com/hiedanrannan.paja" />
+                <SocialIcon className="instagram" url="https://www.instagram.com/hiedanrannan_paja/" />
+                </div>
             </div>
         </div>
        
     )
 }
 
-export default Footer
+export default Footer 
