70d4f2ac79f814de82d264c92ad3933dd797369f
Joni <joni.ylioja@gmail.com>
2019-10-09 11:38:27 +0300

    Added contacts text and artists

--- hrp/src/components/artists/Artists.js
+++ hrp/src/components/artists/Artists.js
@@ -1,15 +1,66 @@
-import React from 'react'
+import React, {Component} from 'react';
 import './Artists.css'
+import { db } from '../../firebase';
 import NavBar from '../nav/NavBar';
 
-export default class Artists extends React.Component {
+class Artists extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            artists: []
+        };
+    }
+
+    componentDidMount() {
+        db.collection("artists")
+            .get()
+            .then(querySnapshot => {
+                const data = querySnapshot.docs.map(doc => doc.data());
+                const id = querySnapshot.docs.map(doc => doc.id);
+                
+                const addId = (data, id) => {
+                    data.forEach(r => r.docName = id[data.indexOf(r)]);
+                    return data;
+                }
+                addId(data, id);
+                
+                console.log(data);
+                
+                this.setState({ artists: data });
+            });
+    }
+
         render() {
-            const title = "Artistit";
-        return (
-            <div>
-                {/* <NavBar title={title}/>   */}
-            </div>
+            const { artists } = this.state;
+            return (
+                <div className="artistRoot">
+                    <div className="container"></div>
+                    <div className="row justify-content-center">  
+                        {artists.map(artist => artist.live === "true" ? (
+                            <div className="card text-white bg-dark mb-4 col-md-4 mx-auto transparent-card">
+                                <img src={require('../../img/placeholder.jpg')} className="card-img-top mt-3" alt="..." />
+                                <div className="card-header"><h2>{artist.name}</h2></div>
+                                <div className="card-body">
+                                    <p className="card-text">
+                                    {artist.description}
+                                    </p>
+                                    <h5 className="card-title">Sähköposti: {artist.email}</h5>
+                                    <h5 className="card-title">
+                                        Osoite: {artist.address}
+                                    </h5>
+                                    <h5 className="card-title">
+                                        Puhelinnumero: {artist.phone}
+                                    </h5>
+                                    <h5 className="card-title">
+                                        <a href={artist.link}>Minun sivut</a>
+                                    </h5>
+                                </div>
+                            </div>
+                        ): null )}
+                    </div>
+                </div>
         )
     }
 }
 
+export default Artists
--- hrp/src/components/contact/Contact.js
+++ hrp/src/components/contact/Contact.js
@@ -8,27 +8,28 @@ export default class Contact extends React.Component {
         render() {
             const title= "Yhteystiedot";
         return (
-<<<<<<< HEAD
-            <div className="contact">
-                <NavBar title={title}/>
-                <div className="vittumitapaskaa"> 
-                    <div class="p-2 flex-fill bd-highlight">
-                        <h1 className="text-left">Näin saavut Hiedanrantaan</h1>
-                        <h2 className="text-left">Kävellen ja pyörällä</h2>
-                        <h2 className="text-left">Bussilla</h2>
-                        <h2 className="text-left">Autolla</h2>
+            <div className="container-fluid">
+                <div className="row">
+                <div className="col-sm-3"></div>
+                    <div className="col-sm-3">
+                        <h2 className="text-left">Näin saavut hiedanrantaan</h2>
+                        <h3 className="text-left">Kävellen ja pyörällä</h3>
+                        <p className="text-left">Kävely- ja pyörätie kulkee Paasikiventien alikulkutunnelilta Lielahden kartanolle ja siitä eteenpäin Niemenrantaan. Kävelijät ja pyöräilijät pääsevät edelleen myös Lielahden kauppa-alueen läpi Lielahdenkadulle, josta käännytään Hiedanrantaan ja tehdasalueelle.<br/><br/>Hiedanrannassa on käytössä muutama EasyBike-pyörä Paasikiventien päädyssä, kartanolla sekä Kuivaamolla.</p>
+                        <h3 className="text-left">Bussilla</h3>
+                        <p className="text-left">Lielahden kartanoa lähin pysäkki on Paasikiventiellä ja se on nimeltään Hiedanranta. Pysäkille pysähtyvät Joukkoliikenteen linjat ovat 3, 14, 21, 28, 37, 71, 80 ja 85. Pysäkiltä on matkaa Kartanolle noin 700 metriä.</p>
+                        <h3 className="text-left">Autolla</h3>
+                        <p className="text-left">Hiedanrantaan pääsee ajamaan autolla ainoastaan Lielahdenkadun kautta. Hiedanrannan alueella on jonkin verran pysäköintitilaa arkikäyttöön. Suurten yleisötapahtumien aikana pysäköinti Hiedanrannan alueella on haasteellista, joten suosittelemme saapumaan silloin julkisilla, pyörällä tai kävellen.</p>
+
                     </div>
-                    <div class="p-2 flex-fill bd-highlight">
-                        <h1 className="text-right">Yhteystiedot</h1>
+                    <div className="col-sm-1"></div>
+                    <div className="col-sm-2">
+                        <h1>Yhteystiedot</h1>
+                        <p className="text-center">Hiedanrannan Paja <br/>Tehdaskartanonkatu 28 <br/> hiedanrannanpaja@gmail.com</p>
+                        <SocialIcon className="facebook" url="http://facebook.com/hiedanrannan.paja" />
+                        <SocialIcon className="instagram" url="https://www.instagram.com/hiedanrannan_paja/" />
                     </div>
                 </div>
-                </div>
-                
-=======
-            <div>
-                {/* <NavBar title={title}/>     */}
             </div>
->>>>>>> 400884966834ba152f318ca5f348830210c3b5a5
         )
     }
 }
