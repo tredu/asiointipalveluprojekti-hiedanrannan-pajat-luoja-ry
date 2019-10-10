21b2c129caaad82529b9a2ed3954392072719bce
Joni <joni.ylioja@gmail.com>
2019-10-10 15:28:40 +0300

    added logo to footer and navbar (still in progress)

--- hrp/src/components/footer/Footer.js
+++ hrp/src/components/footer/Footer.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './Footer.css';
 import { SocialIcon } from 'react-social-icons';
+import Logo from "./hiedanranta-logo.png";
 const Footer  = () => {
     return (    
-        <div className="container p-3 mb-2 text-white">
-            <div className="row">
-                <div className="col-sm" id="footerContent">
+
+        <div className="container p-3 mb-2 text-white" id="footerContent">
+            <div class="row">
+                <div class="col-sm">
+                <img className="img-responsive" src={Logo} alt="hiedanranta-logo"/>
+                </div>
+                <div class="col-sm" id="info">
                 <p className="text-center"><span className="font-weight-bold">Hiedanrannan pajat</span> Tehdaskartanonkatu 28 33400 Tampere</p>
                 <p className="text-center">hiedanrannanpaja@gmail.com</p>
                     <div className="socials">
@@ -13,9 +18,10 @@ const Footer  = () => {
                         <SocialIcon className="instagram" url="https://www.instagram.com/hiedanrannan_paja/" />
                     </div>
                 </div>
+                <div class="col-sm">
+                </div>
             </div>
         </div>
-       
     )
 }
 
