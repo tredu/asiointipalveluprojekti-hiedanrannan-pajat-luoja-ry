84c5aafc5cd3366a743f51371b10dd26d3802d87
Wesku <mrwesku@gmail.com>
2019-10-17 21:50:58 +0300

    NavBar & Admin styling. Courses scrolling menu base done

--- hrp/src/components/admin/Admin.js
+++ hrp/src/components/admin/Admin.js
@@ -5,7 +5,9 @@ import AddEvent from './addEvent'
 import EditEvent from './editEvent'
 import EditArtist from './editArtist'
 import ImageUp from './ImageUp'
-import { Route, Link } from 'react-router-dom'
+import { Route, Link, NavLink } from 'react-router-dom'
+import {Navbar, Nav, NavItem, NavDropdown, MenuItem} from 'react-bootstrap';
+
 import './admin.css'
 
 export default class Admin extends Component {
@@ -45,21 +47,48 @@ export default class Admin extends Component {
     render() {
         return (
             <div className="admin-panel">
-                <h1>Admin panel</h1>
-                <input value={this.state.value} onChange={this.handleChange} />
-                {this.state.value !== this.state.secretWord &&
-                    <div className="adminPanel">
-                        <h1>Welcome admin</h1>
-                        <Link className="nav-link" activeClassName="active" to={"/admin/addartist"}>Lisää Artisti</Link>
-                        <Link className="nav-link" activeClassName="active" to={"/admin/editartist"}>Muokkaa Artistia</Link>
-                        <Link className="nav-link" activeClassName="active" to={"/admin/addevent"}>Lisää Tapahtuma</Link>
-                        <Link className="nav-link" activeClassName="active" to={"/admin/editevent"}>Muokkaa Tapahtumia</Link>
-                        <Route path="/admin/addartist" component={AddArtist} />
-                        <Route path="/admin/editartist" component={EditArtist} />
-                        <Route path="/admin/addevent" component={AddEvent} />
-                        <Route path="/admin/editevent" component={EditEvent} />
-                    </div>
-                }
+                <div className="admin-navbar">
+                    <Navbar collapseOnSelect expand="md" bg="dark" variant="dark" className="transBar">
+                        <Navbar.Brand>Hallintapaneeli</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="mx-auto">
+                            <NavDropdown title="Artistit" id="basic-nav-dropdown">
+                                <NavDropdown.Item className="transBar">
+                                    <Nav.Link as={NavLink} to='/admin/addartist'>Lisää artisti</Nav.Link>
+                                </NavDropdown.Item>
+                                <NavDropdown.Item>
+                                    <Nav.Link as={NavLink} to='/admin/editartist'>Muokkaa artistia</Nav.Link>
+                                </NavDropdown.Item>
+                            </NavDropdown>
+                            <NavDropdown title="Tapahtumat" id="basic-nav-dropdown">
+                                <NavDropdown.Item className="transBar">
+                                    <Nav.Link as={NavLink} to='/admin/addevent'>Lisää tapahtuma</Nav.Link>
+                                </NavDropdown.Item>
+                                <NavDropdown.Item>
+                                    <Nav.Link as={NavLink} to='/admin/editevent'>Muokkaa tapahtumaa</Nav.Link>
+                                </NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+                    </Navbar.Collapse>
+                    </Navbar>
+                </div>
+                <div className="adminPanel">
+                    <Route path="/admin/addartist" component={AddArtist} />
+                    <Route path="/admin/editartist" component={EditArtist} />
+                    <Route path="/admin/addevent" component={AddEvent} />
+                    <Route path="/admin/editevent" component={EditEvent} />
+
+
+                    {/* <Link className="nav-link" activeClassName="active" to={"/admin/addartist"}>Lisää Artisti</Link>
+                    <Link className="nav-link" activeClassName="active" to={"/admin/editartist"}>Muokkaa Artistia</Link>
+                    <Link className="nav-link" activeClassName="active" to={"/admin/addevent"}>Lisää Tapahtuma</Link>
+                    <Link className="nav-link" activeClassName="active" to={"/admin/editevent"}>Muokkaa Tapahtumia</Link>
+                    <Route path="/admin/addartist" component={AddArtist} />
+                    <Route path="/admin/editartist" component={EditArtist} />
+                    <Route path="/admin/addevent" component={AddEvent} />
+                    <Route path="/admin/editevent" component={EditEvent} /> */}
+                </div>
             </div>
         )
     }
